f8167440e389141ad1aff021b28e39dcc44254e5
2008-manya <72509525+2008-manya@users.noreply.github.com>
2022-01-25 17:21:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function bedroom(){
+    window.location="bedroom.html";
+}
+
+function table(){
+    window.location="table.html";
+}
+
+function bottles(){
+    window.location="bottle.html";
+}
+
+function tv_ac(){
+    window.location="tv_ac.html";
+}
